8f747aa9a46550b8729d017c88a48f4342b5489e
leeja120 <jlee.jasoftsolutions@gmail.com>
2014-12-19 19:55:43 -0500

    diseño y funcionalidad basica terminada

--- levelbook/app/src/main/java/com/jasoftsolutions/levelbook/MainActivity.java
+++ levelbook/app/src/main/java/com/jasoftsolutions/levelbook/MainActivity.java
@@ -1,29 +1,87 @@
 package com.jasoftsolutions.levelbook;
 
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private final static String TAG = "Debug";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Integer numeroEjercicio = (Integer) 1;
+        Integer numeroEjercicio = 1;
+        final Integer numero1 = (int) (Math.random() * 10);
+        final Integer numero2 = (int) (Math.random() * 10);
+
         String stringNumeroEjercicio = getResources().getString(R.string.tvNumeroEjercicio) + " " + numeroEjercicio.toString();
 
         TextView tvNumeroEjercicio = (TextView) findViewById(R.id.tvNumeroEjercicio);
         tvNumeroEjercicio.setText(stringNumeroEjercicio);
 
+        TextView tvNumero1 = (TextView) findViewById(R.id.tvNumero1);
+        TextView tvNumero2 = (TextView) findViewById(R.id.tvNumero2);
 
+        tvNumero1.setText(numero1.toString());
+        tvNumero2.setText(numero2.toString());
+
+        Button btVerificar = (Button) findViewById(R.id.btVerificar);
+
+        final EditText etRespuesta = (EditText) findViewById(R.id.etRespuesta);
+
+        btVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Log.e(TAG, etRespuesta.getText().toString());
+
+                if(etRespuesta.getText().toString() != null &&  (!etRespuesta.getText().toString().isEmpty())){
+                    Integer resultado, respuesta;
+
+                    resultado = numero1 * numero2;
+                    respuesta = Integer.parseInt(etRespuesta.getText().toString());
+                    if(resultado == respuesta){
+                        Toast.makeText(getApplicationContext(), "Bien", Toast.LENGTH_SHORT).show();
+                    /*
+                    numeroEjercicio++;
+
+                    stringNumeroEjercicio = getResources().getString(R.string.tvNumeroEjercicio) + " " + numeroEjercicio[0].toString();
+                    tvNumeroEjercicio.setText(stringNumeroEjercicio);
+
+                    numero1 = (int) (Math.random()*10);
+                    numero2 = (int) (Math.random()*10);
+
+                    tvNumero1.setText(numero1[0].toString());
+                    tvNumero2.setText(numero2[0].toString());
+                    */
+
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Mal", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Ingrese Número", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
 
         //Toast.makeText(getBaseContext(), numeroEjercicio, Toast.LENGTH_LONG).show();
+
 
     }
 
